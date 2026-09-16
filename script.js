c7676f1bfd87bdcdf10fc3ea6780c24f5f1972d1
Segni Mideksa <segnim21@gmail.com>
2026-09-16 10:25:04 +0300

    Fix weather visibility bug on search

--- script.js
+++ script.js
@@ -20,3 +20,56 @@ const weatherCodes = {
   71: "Light snow",
   80: "Rain showers",
 };
+
+// event listeners for form submit
+searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let city = cityInput.value;
+  cityInput.value = "";
+  fetchWeather(city);
+});
+
+//the main async function
+async function fetchWeather(city) {
+  errorMessage.style.display = "none";
+  weatherDisplay.style.display = "none";
+
+  try {
+    let geoResponse = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`,
+    );
+    let geoData = await geoResponse.json();
+
+    //check for server found city
+    if (!geoData.results || geoData.results.length === 0) {
+      throw new Error(
+        "City not found. Please double check spelling of your city.",
+      );
+    }
+    let latitude = geoData.results[0].latitude;
+    let longitude = geoData.results[0].longitude;
+    let actualCityName = geoData.results[0].name;
+
+    //fetch weather using coordinates
+
+    let weatherResponse = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`,
+    );
+    let weatherData = await weatherResponse.json();
+
+    //now the data we need is getting by following
+
+    let currentTemp = weatherData.current_weather.temperature;
+    let currentCode = weatherData.current_weather.weathercode;
+    let descText = weatherCodes[currentCode] || "Unknown weather";
+
+    //update DOM elements
+
+    cityName.textContent = actualCityName;
+    temperature.textContent = `${currentTemp}°C`;
+    description.textContent = descText;
+  } catch (error) {
+    errorMessage.textContent = error.message;
+    errorMessage.style.display = "block";
+  }
+}
